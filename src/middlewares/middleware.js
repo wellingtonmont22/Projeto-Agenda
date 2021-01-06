dbeb8a8503a43497f8e6c9beaf8cd09ed9222247
wellingtonmont22 <wel345@hotmail.com>
2021-01-06 19:59:58 -0300

    Criação de pagina, rota e controller da pagina de login

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -5,9 +5,10 @@ exports.middlewareGlobal = (req, res, next) => {
 }
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && err.code === 'EBADCSRFTOKEN'){
+    if(err){
         return res.render('404');
     }
+    next();
 }
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
