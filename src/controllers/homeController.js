8ebf99109202d1d8f6b1291a65766e79879021c7
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-03-07 11:40:45 -0300

    privando a agenda de quem não está logado e alteração de textos

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,14 @@
+const { loginRequired } = require('../middlewares/middleware');
 const Contato = require('../models/ContatoModel');
 exports.index = async function(req, res) {
-    /* Por buscaContatos ser um metodo estatico não precisa ser instanciado*/
-    const contatos = await Contato.buscaContatos();
-    res.render('index', { contatos });
+    try{
+        
+        /* Por buscaContatos ser um metodo estatico não precisa ser instanciado*/
+        const contatos = await Contato.buscaContatos();
+        res.render('index', { contatos });
+    }catch(e){
+        console.log(e);
+        res.render('404');
+    }
+
 };
